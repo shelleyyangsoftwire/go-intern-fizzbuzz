68f9a4f2e8feea41aa437fdc52ae82ce56ecf347
SheYan <shelley.yang@softwire.com>
2025-07-08 13:47:11 +0100

    basic

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import "fmt"
 
+var rules = []struct {
+	multiple int
+	word     string
+}{
+	{3, "Fizz"},
+	{5, "Buzz"},
+}
+
 // This is our main function, this executes by default when we run the main package.
 func main() {
 	fmt.Println("Hello, World!")
 
 	// Put your code here...
+
+	for i := 1; i <= 100; i++ {
+		word := ""
+		for _, rule := range rules {
+			if i%rule.multiple == 0 {
+				word += rule.word
+			}
+		}
+		if word != "" {
+			fmt.Println(word)
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
